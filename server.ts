36b7e1db70f73c4a9e4fb0c4877b7e0600b633c9
Ifeoluwa00dev <ifeoluwathedaddy@gmail.com>
2026-06-09 18:39:06 +0100

    add cors headers

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://wc-26-tau.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
